c17483c99fc12bfafd0820283983af363998b67a
Abhishek Mishra <82857542+mishra-31@users.noreply.github.com>
2023-02-10 16:32:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route("/showdata", methods=["POST"])
+def receive_data():
+    data = request.get_json()
+    #print(data)
+    return data
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
